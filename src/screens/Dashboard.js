538d94c2d350bcf90239efcf2dbab71254efacb1
Elielton Agostinho <elieltonagostinho@edu.unifor.br>
2021-08-19 11:21:17 -0300

    Projeto Com Face ID e teste de conxao

---
+++ src/screens/Dashboard.js
@@ -0,0 +1,95 @@
+import React,{useState} from 'react'
+import Background from '../components/Background'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Paragraph from '../components/Paragraph'
+import Button from '../components/Button'
+import { Alert, Modal, Platform, Touchable, View,StyleSheet } from 'react-native'
+
+import * as LocalAuthentication  from 'expo-local-authentication';
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { styleSheets } from 'min-document'
+ 
+export default function Dashboard({ navigation }) {
+
+  const [isModalVisible, setIsModalVisible] = useState(true);
+
+  async function authenticate(){
+    const hasPassword = await LocalAuthentication.isEnrolledAsync();
+    if(!hasPassword) return;
+
+    const {success, error} = await LocalAuthentication.authenticateAsync();
+
+    if (success) {
+      Alert.alert('Autenticação realizada com sucesso!');
+    }else{
+      Alert.alert('Error!');
+    }
+    setIsModalVisible(false);
+  }
+
+  
+  function btnMarcarPresenca(){
+    if(Platform.OS === 'ios'){
+      authenticate();
+    }else{
+      Platform.OS === 'android' && (
+        <Modal
+          animationType='slide'
+          transparent={true}
+          visible={isModalVisible}
+          onShow={authenticate}
+        >
+          <View style={styleSheets.modal}>
+            <Text style={styleSheets.authText}>Autentique-se utilizando sua digital</Text>
+            <TouchableOpacity onPress={() =>{
+            LocalAuthentication.cancelAuthenticate();
+            setIsModalVisible(false);
+            }}>
+            <Text style={styleSheets.cancel}>Cancelar</Text>
+          </TouchableOpacity>
+          </View>
+          
+        </Modal>
+      );
+    }
+  }
+
+  return (
+    <Background>
+      <Logo />
+      <Header>Let’s start</Header>
+      <Paragraph>
+        Your amazing app starts here. Open you favorite code editor and start
+        editing this project.
+      </Paragraph>
+      <Button
+        mode="outlined"
+        onPress={btnMarcarPresenca}
+      >
+        Marcar Presença
+      </Button>
+    </Background>
+  )
+}
+
+const style = StyleSheet.create({
+  modal:{
+    backgroundColor: '#333',
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height:'40%'
+  },
+  cancel:{
+    color:'red',
+    fontSize: 16
+  },
+  authText:{
+    color:'white',
+    fontSize: 16
+  }
+});
---
+++ src/screens/StartScreen.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import { Image, StyleSheet,KeyboardAvoidingView, View, ImageBackground } from 'react-native'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import Paragraph from '../components/Paragraph'
+
+export default function StartScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+    <View style={styles.bgd}>
+    <ImageBackground
+      source={require('../assets/background_dot2x.png')}
+      resizeMode={'repeat'}
+      style={styles.background}/>  
+    </View>
+
+    <View style={styles.dados}>
+      
+    <Image source={require('../assets/UniPoint-unscreen.gif')} style={styles.image} />
+    <Paragraph>
+      The easiest way to start with your amazing application.
+    </Paragraph>
+    <Button
+      mode="contained"
+      onPress={() => navigation.navigate('LoginScreen')}
+    >
+      Entrar
+    </Button>
+    <Button
+      mode="outlined"
+      onPress={() => navigation.navigate('RegisterScreen')}
+    >
+      Cadastrar
+    </Button>
+  
+  </View>
+  </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  bgd: {
+    flex: 0,
+    width: '100%',
+    maxWidth: 340,
+    backgroundColor:'#000000',
+    opacity:1,
+  },
+  image: {
+    width: 350,
+    height: 110,
+    marginBottom: 8,
+  },
+  dados: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft:80,
+    padding: 10,
+    maxWidth: 340,
+    backgroundColor:'transparent',
+  },
+  background: {
+    flex: 1,
+    maxWidth: 340,
+    width: 600,
+    height: 1024,
+  },
+  container: {
+    flex: 1,
+    width: '100%',
+    maxWidth: 340,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor:'transparent',
+    
+  },
+})
